510ee05a8f58d501aa90674dc177fcd5f5b4be34
Norayio <pierresediaz@gmail.com>
2024-01-15 20:47:03 -0500

    primer commit de la app gestion-de-clientes

---
+++ main/java/com/mycompany/prueba1/Prueba1.java
@@ -0,0 +1,32 @@
+/*
+
+ */
+package com.mycompany.prueba1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Norayio
+ */
+public class Prueba1 {
+
+    public static void main(String[] args) {
+        
+        Formulario panta = new Formulario();
+        
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
